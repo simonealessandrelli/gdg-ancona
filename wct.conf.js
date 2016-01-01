d80294e6d779ffca4cfff197c987ab217ba41bb2
Simone <simonealessandrelli@gmail.com>
2016-01-01 19:30:14 +0100

    Add travis support!

--- wct.conf.js
+++ wct.conf.js
@@ -16,45 +16,20 @@ var ret = {
       disabled: true,
       commandTimeout: 600 * 10,
       idleTimeout: 180,
-      username: 'elmar1ach1',
-      accessKey: 'fbc36797-58ca-4295-b710-4530637df598',
+      username: 'simonealessandrelli',
+      accessKey: 'ecb73d2b-3591-488e-af3c-c866370f3f32',
       browsers: [
         /* see https://docs.saucelabs.com/reference/platforms-configurator */
-        /*{
-          browserName: 'chrome',
-          platform: 'OS X 10.9',
-          version: '40'
-        },
         {
-          browserName: 'safari',
-          platform: 'OS X 10.9',
-          version: '7.0'
+          browserName: 'microsoftedge',
+          platform: 'Windows 10',
+          version: ''
         },
-        {
-          browserName: 'safari',
-          platform: 'OS X 10.10',
-          version: '8.0'
-        },
-        {
-          browserName: 'chrome',
-          platform: 'Windows 8.1',
-          version: '40'
-        },*/
         {
           browserName: 'internet explorer',
           platform: 'Windows 8.1',
           version: '11.0'
-        },/*
-        {
-          browserName: 'firefox',
-          platform: 'Windows 7',
-          version: '27.0'
-        },
-        {
-          browserName: 'internet explorer',
-          platform: 'Windows 7',
-          version: '10.0'
-        }*/
+        }
       ]
     }
   }
