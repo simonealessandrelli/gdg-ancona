546e1be06fbff88fb4c4c1a79a5761344f3c906f
Simone <simonealessandrelli@gmail.com>
2016-01-07 04:05:55 +0100

    Readme with build badge

--- wct.conf.js
+++ wct.conf.js
@@ -6,28 +6,43 @@ var ret = {
     'pathMappings': []
   },
   plugins: {
-    'local': {
-      disabled: false,
+    local: {
+      disabled: true,
       browsers: ['chrome', 'firefox']
     },
-    'sauce': {
-      disabled: false,
+    sauce: {
+      disabled: true,
       commandTimeout: 600,
       idleTimeout: 180,
       username: 'simonealessandrelli',
       accessKey: 'ecb73d2b-3591-488e-af3c-c866370f3f32',
       browsers: [
         /* see https://docs.saucelabs.com/reference/platforms-configurator */
+        {
+          browserName: 'chrome',
+          platform: 'Windows 10',
+          version: '43'
+        },
+        {
+          browserName: 'chrome',
+          platform: 'linux',
+          version: '43'
+        },
+        {
+          browserName: 'chrome',
+          platform: 'OS X 10.11',
+          version: '43'
+        },
         // {
         //   browserName: 'microsoftedge',
         //   platform: 'Windows 10',
         //   version: ''
         // },
-        {
-          browserName: 'internet explorer',
-          platform: 'Windows 8.1',
-          version: '11.0'
-        }
+        // {
+        //   browserName: 'internet explorer',
+        //   platform: 'Windows 8.1',
+        //   version: '11.0'
+        // }
       ]
     }
   }
